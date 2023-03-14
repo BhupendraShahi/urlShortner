b5a289cc50301b8d56cf9bae63c56732d8b5531d
bhupendra Singh shahi <shahi23.bhupendra11@gmail.com>
2023-03-14 08:59:41 +0530

    chaged base url

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL: process.env.BASE_URL + "/auth/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
